e7c8707f5743885b90775bc6c6746d01aa301411
artemkaplagg <artemkacurovskij36@gmail.com>
2026-01-12 00:13:41 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,1 +1,7 @@
+from .start import router as start_router
+from .rates import router as rates_router
+from .alerts import router as alerts_router
+from .admin import router as admin_router
+
+__all__ = ['start_router', 'rates_router', 'alerts_router', 'admin_router']
 
